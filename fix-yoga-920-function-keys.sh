7f823c795b60193f720eacf50b1a04276fd61709
Martin Lütken <martin@lutken.dk>
2019-10-14 14:34:23 +0200

    Fixed script permissions

--- fix-yoga-920-function-keys.sh
+++ fix-yoga-920-function-keys.sh
@@ -2,17 +2,21 @@
 IDEAPAD_HACKS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 IDEAPAD_HACKS_PATH=${IDEAPAD_HACKS_DIR}/ideapad-hacks.ko
 
+sudo rm -rf .tmp_versions/
 sudo rmmod ideapad-hacks.ko
 
 pushd ${IDEAPAD_HACKS_DIR}
 echo "Fixing Yga 920 function keys"
 echo "IDEAPAD_HACKS_PATH: ${IDEAPAD_HACKS_PATH}"
-make clean
-make
+sudo make clean
+sudo make
 
 echo "IDEAPAD_HACKS_PATH: ${IDEAPAD_HACKS_PATH}"
 sudo insmod ${IDEAPAD_HACKS_PATH}
-device_path=$(find /sys -name fn_lock)
-sudo echo 0 > ${device_path}
+device_path=$(sudo find /sys -name fn_lock)
+echo "device_path: ${device_path}"
+sudo sh -c "echo '0' > ${device_path}"
 
 popd
+
+
